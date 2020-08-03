99c6fb3f9b6e99e2e8f6fc49699c1623a7be78ee
fltbuff1792 <63353736+fltbuff1792@users.noreply.github.com>
2020-08-03 07:41:27 -0400

    re applying after changing rep name

--- commit  to git.bat
+++ commit  to git.bat
@@ -3,7 +3,7 @@ echo on
 cd c:\users\fltbu\documents\harvard\
 cd Project0
 
- git  commit -m "Updates all files for project reqs except last several: variables, nesting and inheritance"
+ git  commit -m "re applying after changing rep name"
 
 pause
 
